9086df99adbb0402669edf08260976e56c3801e9
Sasaki Haruna <sasaki.haruna@flymee.co.jp>
2026-03-06 11:51:02 +0900

    コマンド管理設定及びPrettierによるコード整形

--- src/typescript/main.ts
+++ src/typescript/main.ts
@@ -1,7 +1,7 @@
 // SCSSの読み込み
-import '../scss/style.scss';
+import "../scss/style.scss";
 
 // 各機能の読み込み
-import './navigation';
-import './modal';
-import './scroll';
+import "./navigation";
+import "./modal";
+import "./scroll";
